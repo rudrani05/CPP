ad798bb2486e4996c4242f8a170222e864cbaaf3
Rudrani <60399242+rudrani05@users.noreply.github.com>
2021-05-10 11:12:39 +0530

    Add files via upload

---
+++ C++/input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int am1;
+    cin>>am1;
+    int am2;
+    cin>>am2;
+    int sum=am1+am2;
+    cout<<"Sum "<<sum<<endl;
+    return 0;
+
+    
+}
