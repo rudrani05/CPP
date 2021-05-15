39201b7ca7fef3545283fddb3c688248f34ae1f8
Rudrani <60399242+rudrani05@users.noreply.github.com>
2021-05-15 09:23:46 +0530

    Add files via upload

---
+++ C++/pattern6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// Floyd's Triangle
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            count+=1;
+            cout<<count<<" ";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
